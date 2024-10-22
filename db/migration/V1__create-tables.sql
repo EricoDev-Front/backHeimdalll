afc4031a79612a828e094ea7ba214d66290209e4
Gustavo <gmassamichi@gmail.com>
2024-10-21 21:11:47 -0300

    save

--- db/migration/V1__create-tables.sql
+++ db/migration/V1__create-tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE Sala (
     num_mesas INT,
     num_projetores INT,
     num_computadores INT,
-    num_lousas INT,
+    num_lousas INT
 );
 
 CREATE TABLE Professor (
@@ -17,7 +17,7 @@ CREATE TABLE Professor (
     email VARCHAR(50) NOT NULL,
     senha VARCHAR(50) NOT NULL,
     registro VARCHAR(10) NOT NULL,
-    adm BOOLEAN NOT NULL,
+    adm BOOLEAN NOT NULL
 );
 
 CREATE TABLE Aluno (
@@ -26,13 +26,13 @@ CREATE TABLE Aluno (
     email VARCHAR(50) NOT NULL,
     senha VARCHAR(50) NOT NULL,
     registro VARCHAR(10) NOT NULL,
-    ano_entrada INT NOT NULL,
+    ano_entrada INT NOT NULL
 );
 
 CREATE TABLE Disciplina (
     disciplina_id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    descricao VARCHAR(300),
+    descricao VARCHAR(300)
 );
 
 CREATE TABLE Curso (
@@ -72,7 +72,7 @@ CREATE TABLE Turma (
     professor_id INT NOT NULL,
     aluno_id INT NOT NULL,
     disciplina_id INT NOT NULL,
-    periodo DATETIME NOT NULL,
+    periodo VARCHAR(10) NOT NULL,
     FOREIGN KEY (professor_id) REFERENCES Professor(professor_id),
     FOREIGN KEY (aluno_id) REFERENCES Aluno(aluno_id),
     FOREIGN KEY (disciplina_id) REFERENCES Disciplina(disciplina_id)
@@ -85,3 +85,4 @@ CREATE TABLE Interesse (
     FOREIGN KEY (aluno_id) REFERENCES Aluno(aluno_id),
     FOREIGN KEY (turma_id) REFERENCES Turma(turma_id)
 );
+
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import * as fs from 'fs';
 import * as https from 'https';
 
 async function bootstrap() {
-
-   // Carregar os certificados do Let's Encrypt
-   const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/heimdallback.eastus2.cloudapp.azure.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/heimdallback.eastus2.cloudapp.azure.com/fullchain.pem'),
+  // Carregar os certificados do Let's Encrypt
+  const httpsOptions = {
+    key: fs.readFileSync(
+      '/etc/letsencrypt/live/heimdallback.eastus2.cloudapp.azure.com/privkey.pem',
+    ),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/heimdallback.eastus2.cloudapp.azure.com/fullchain.pem',
+    ),
   };
 
   const app = await NestFactory.create(AppModule, {
@@ -21,7 +24,9 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('API do Projeto Heimdall')
-    .setDescription('Documentação da API para gerenciamento de reservas de salas')
+    .setDescription(
+      'Documentação da API para gerenciamento de reservas de salas',
+    )
     .setVersion('1.0')
     .addTag('reservas')
     .build();
@@ -29,11 +34,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); // Acesso à documentação em http://localhost:3000/api
 
-
   // Iniciar o servidor HTTPS na porta 443
   await app.listen(443, () => {
     console.log('Servidor HTTPS rodando na porta 443');
   });
 }
 bootstrap();
-
