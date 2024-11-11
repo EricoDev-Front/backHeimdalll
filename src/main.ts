dc96a6c2fa38917adf9ed0be86f2b0f56f56620e
Érico Vinícius <ericovinicius333@gmail.com>
2024-11-11 13:11:36 -0300

    fix: correção porta 443

--- src/main.ts
+++ src/main.ts
@@ -35,11 +35,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document); // Acesso à documentação em http://localhost:3000/api
 
   // Iniciar o servidor HTTPS na porta 443
- // await app.listen(443, () => {
-  //  console.log('Servidor HTTPS rodando na porta 443');
- // });
- await app.listen(3000, () => {
-    console.log('Servidor HTTP rodando na porta 3000');
-  });
+ await app.listen(443, () => {
+   console.log('Servidor HTTPS rodando na porta 443');
+ });
 }
 bootstrap();
