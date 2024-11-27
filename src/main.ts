ffc7b39d238ccec4edfd1badbe3721c43f4ec2a1
gustavofalconi <gustavo123755@gmail.com>
2024-11-27 19:33:32 -0300

    habilitar  https

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,42 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'reflect-metadata';
+import * as fs from 'fs';
+import * as https from 'https';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Carregar os certificados do Let's Encrypt
+  const httpsOptions = {
+    key: fs.readFileSync(
+      '/etc/letsencrypt/live/heimdallback.eastus2.cloudapp.azure.com/privkey.pem',
+    ),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/heimdallback.eastus2.cloudapp.azure.com/fullchain.pem',
+    ),
+  };
 
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  app.enableCors(); // Habilita o CORS
 
   const config = new DocumentBuilder()
     .setTitle('API do Projeto Heimdall')
-    .setDescription('Documentação da API para gerenciamento de reservas de salas')
+    .setDescription(
+      'Documentação da API para gerenciamento de reservas de salas',
+    )
     .setVersion('1.0')
     .addTag('reservas')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document); // Acesso à documentação em http://localhost:3000/api
 
-  await app.listen(3000);
-  console.log('Servidor rodando em http://localhost:3000');
+  // Iniciar o servidor HTTPS na porta 443
+  await app.listen(443, () => {
+    console.log('Servidor HTTPS rodando na porta 443');
+  });
 }
-
 bootstrap();
